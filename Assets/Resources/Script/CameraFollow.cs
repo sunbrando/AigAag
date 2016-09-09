04fa5df4c394525ef117560f474e55467871fa23
sunbrando <k_xun@qq.com>
2016-09-09 23:48:17 +0800

    Came update

--- Assets/Resources/Script/CameraFollow.cs
+++ Assets/Resources/Script/CameraFollow.cs
@@ -6,31 +6,41 @@ using System.Collections;
 /// </summary>
 public class CameraFollow : MonoBehaviour {
 
+    //目标
     public GameObject followTarget;
 
+    //摄像机默认位置
     private Vector3 mCamFirstPos;
-    private Vector3 mLastPosition;
+    //目标最后一次位置
+    private Vector3 mLastTagPosition;
+    //目标开始时的位置
+    private Vector3 mFirstTagPosition;
 
 	// Use this for initialization
 	void Start () {
         mCamFirstPos = transform.position;
-        mLastPosition = followTarget.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
+        mLastTagPosition = followTarget.transform.position;
+        mFirstTagPosition = followTarget.transform.position;
 	}
 
     void LateUpdate()
     {
-        //float deltaPosZ = followTarget.transform.position.z - mLastPosition.z;
-        //mLastPosition = followTarget.transform.position;
-        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + deltaPosZ);
-        if (mLastPosition.y == followTarget.transform.position.y)
+        if (mFirstTagPosition.y == followTarget.transform.position.y)
         {
-            transform.position = new Vector3(mCamFirstPos.x + followTarget.transform.position.x, mCamFirstPos.y + followTarget.transform.position.y, mCamFirstPos.z + followTarget.transform.position.z);
+            transform.position = UpdateCamPosition(mCamFirstPos, followTarget.transform.position, mLastTagPosition, Time.deltaTime);
         }
+        mLastTagPosition = followTarget.transform.position;
+    }
 
+    //更新相机的位置
+    Vector3 UpdateCamPosition(Vector3 camFirstPos, Vector3 tagNowPos, Vector3 tagLastPos, float deltaTime)
+    {
+        //float deltaPosX = Mathf.Lerp(0, tagNowPos.x - tagLastPos.x, deltaTime);
+        //float deltaPosZ = Mathf.Lerp(0, tagNowPos.z - tagLastPos.z, deltaTime);
+        //Vector3 position = new Vector3(camFirstPos.x + tagLastPos.x + deltaPosX, camFirstPos.y + tagLastPos.y, camFirstPos.z + tagLastPos.z + deltaPosZ);
+
+        Vector3 position = new Vector3(camFirstPos.x + tagNowPos.x, camFirstPos.y + tagNowPos.y, camFirstPos.z + tagNowPos.z);
+
+        return position;
     }
 }
